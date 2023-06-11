2aa85f69959fc3f0c99fa5dfe52c15e90c303c90
Juan Suarez <jp.juansuarez@gmail.com>
2023-06-11 01:54:08 -0300

    Added exercise 9

---
+++ 4.1/Ejercicio_9.py
@@ -0,0 +1,17 @@
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+num3 = int(input("Ingrese el tercer número: "))
+
+if num1 >= num2 and num1 >= num3:
+    mayor = num1
+elif num2 >= num1 and num2 >= num3:
+    mayor = num2
+else:
+    mayor = num3
+
+print("El número mayor es:", mayor)
+
+if mayor % 2 == 0:
+    print("El número mayor es par.")
+else:
+    print("El número mayor es impar.")
