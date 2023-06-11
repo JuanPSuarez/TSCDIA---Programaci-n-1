92bee289461737c511c5dd448052a52d8c262039
Juan Suarez <jp.juansuarez@gmail.com>
2023-06-11 01:33:07 -0300

    Added exercise 3

---
+++ 4.1/Ejercicio_3.py
@@ -0,0 +1,8 @@
+genero = input("Ingrese su género (f/m): ")
+
+if genero == "f":
+    print("Vota en mesa femenina.")
+elif genero == "m":
+    print("Vota en mesa masculina.")
+else:
+    print("Género no válido. Ingrese 'f' o 'm'.")
