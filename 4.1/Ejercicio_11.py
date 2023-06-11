486a812790a415a7063f9c149d5ef4c19a025737
Juan Suarez <jp.juansuarez@gmail.com>
2023-06-11 01:55:42 -0300

    Added exercise 11

---
+++ 4.1/Ejercicio_11.py
@@ -0,0 +1,30 @@
+numero = int(input("Ingrese un número del 1 al 12: "))
+
+if numero == 1:
+    mes = "Enero"
+elif numero == 2:
+    mes = "Febrero"
+elif numero == 3:
+    mes = "Marzo"
+elif numero == 4:
+    mes = "Abril"
+elif numero == 5:
+    mes = "Mayo"
+elif numero == 6:
+    mes = "Junio"
+elif numero == 7:
+    mes = "Julio"
+elif numero == 8:
+    mes = "Agosto"
+elif numero == 9:
+    mes = "Septiembre"
+elif numero == 10:
+    mes = "Octubre"
+elif numero == 11:
+    mes = "Noviembre"
+elif numero == 12:
+    mes = "Diciembre"
+else:
+    mes = "Número inválido"
+
+print("El nombre del mes correspondiente es:", mes)
